26c2e332b1431ec8f61262eb6374652fd0fc95d7
Katie Lin <katielin019@gmail.com>
2023-09-12 15:31:19 -0400

    Played with date playground

---
+++ app/src/main/java/com/example/tempconverterstopwatch/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.tempconverterstopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private var startTime: Long = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickStart(view: View) {
+
+    }
+
+    fun onClickStop(view: View) {
+
+    }
+}
